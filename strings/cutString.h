2ab316da826e3d5c53cbbe6e8fb16b8613147bcb
Gustavo <gunaldoni@gmail.com>
2022-09-28 12:01:18 -0300

    Add function to cut part of string

---
+++ strings/cutString.h
@@ -0,0 +1,24 @@
+#include <string.h>
+
+void CutString(char *, char *, int, int);
+
+void CutString(char *string, char *result, int init, int end)
+{
+    int i, j;
+    int resSize = end - init + 1;
+
+    j = 0;
+    for (i = 0; i < strlen(string) + 1; i++)
+    {
+        if (i >= init && i <= end)
+        {
+            result[j] = string[i];
+            j++;
+        }
+
+        if (i == end + 1)
+        {
+            result[j] = '\0';
+        }
+    }
+}
