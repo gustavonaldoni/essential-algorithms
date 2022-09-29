4e026356b43042931ecce5b6c086eb62265c75b3
Gustavo <gunaldoni@gmail.com>
2022-09-29 13:51:13 -0300

    Add function to cut string (needed for StringTrim())

--- strings/trimString.h
+++ strings/trimString.h
@@ -5,6 +5,27 @@
 void StringCut(char*, char*, int, int);
 void StringTrim(char*, char*);
 
+void StringCut(char *string, char *result, int init, int end)
+{
+    int i, j;
+    int resSize = end - init + 1;
+
+    j = 0;
+    for (i = 0; i < strlen(string) + 1; i++)
+    {
+        if (i >= init && i <= end)
+        {
+            result[j] = string[i];
+            j++;
+        }
+
+        if (i == end + 1)
+        {
+            result[j] = '\0';
+        }
+    }
+}
+
 void StringTrim(char* string, char* result)
 {
     int i;
