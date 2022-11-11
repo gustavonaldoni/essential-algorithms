87b1c15b76d4a40814d60cf12ddc54520ab8dabe
gustavonaldoni <gunaldoni@gmail.com>
2022-11-11 16:36:16 -0300

    Improve StringConcat() efficiency and create StringReplace(), which needs bug fixing

--- strings/string_type.h
+++ strings/string_type.h
@@ -54,7 +54,7 @@ String StringConcat(String source, String destination)
 {
     size_t lengthSource, lengthDestination, lengthResult, i;
     String resultString;
-    char *resultContent, *auxDestination;
+    char *resultContent;
 
     lengthSource = strlen(source.content);
     lengthDestination = strlen(destination.content);
@@ -77,6 +77,76 @@ String StringConcat(String source, String destination)
     resultContent[lengthResult] = '\0';
 
     resultString = StringCreate(resultContent);
+
+    free(resultContent);
+
+    return resultString;
+}
+
+String StringReplace(String original, String toReplace, String replaceBy)
+{
+    size_t lengthOriginal, lengthToReplace, lengthReplaceBy, lengthResultString, i, j;
+    String resultString;
+    char *resultContent;
+    unsigned short int counter, foundWord;
+    short int firstLetterIndex, lastLetterIndex;
+
+    resultString.content = NULL;
+
+    counter = 0; 
+    foundWord = 0;
+
+    firstLetterIndex = -1; 
+    lastLetterIndex = -1;
+
+    lengthToReplace = strlen(toReplace.content);
+    lengthOriginal = strlen(original.content);
+    
+    j = 0;
+
+    // Verify where, at 'original', we have the string 'toReplace'
+    for (i = 0; i < lengthOriginal - lengthToReplace + 1; i++)
+    {
+        if (original.content[i] == toReplace.content[j])
+        {
+            counter += 1;
+            j += 1;
+        }
+            
+        if (counter == lengthToReplace)
+        {
+            lastLetterIndex = i;
+            foundWord = 1;
+            break;
+        }
+            
+        if (original.content[i] != toReplace.content[i])
+        {
+            counter = 0; 
+            j = 0;
+        }
+                 
+    }
+
+    if (foundWord)
+    {
+        firstLetterIndex = lastLetterIndex - lengthToReplace + 1;
+        lengthResultString = lengthOriginal + (lengthToReplace - lengthReplaceBy);
+
+        resultContent = (char *) malloc(lengthResultString);
+
+        for (i = 0; i < lengthResultString; i++)
+        {
+            if (i >= firstLetterIndex && i <= lastLetterIndex)
+                resultContent[i] = replaceBy.content[i];
+
+            else
+                resultContent[i] = original.content[i];
+        }
+
+        resultContent[lengthResultString] = '\0';
+        resultString = StringCreate(resultContent);
+    }
 
     return resultString;
 }
